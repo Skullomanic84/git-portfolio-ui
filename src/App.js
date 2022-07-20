93d2516ed62729b373f712a8703d2fa5c3af1e42
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-07-20 13:44:44 +0200

    SingleProject.jsx
    created and populated the SingleProject page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import logo from './assets/logo.svg';
 import './App.css';
 import Profile from './pages/Profile';
+import Projects from './pages/Projects'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header logo={logo}/>
-      <Profile userName="Skullomanic84"/>
+      {/* routing */}
+      <Routes>
+        <Route path='/' element={<Profile userName="Skullomanic84" />} />
+        <Route path='/projects' element={<Projects userName="Skullomanic84" />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import './Header.css'
 
 function Header({ logo }) {
   return (
     <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo'
-            />
-            <h3>My Git Profile</h3>
+        <img 
+        src={logo} 
+        className='App-logo' 
+        alt='logo'/>
+
+        <h3>My Git Profile</h3>
+
+        <nav>
+          <RouterLink to='/' className='App-link'>
+            About Me
+          </RouterLink>
+          <RouterLink to='/' className='App-link'>
+            Projects
+          </RouterLink>
+        </nav>
             
     </header>
-  )
+  );
 }
 
 export default Header;
